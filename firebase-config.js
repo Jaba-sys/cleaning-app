6db9a57e39c8df2806dda7068ac99fd1c7414b35
Jaba-sys <Sop3chit.xpol.viva0@gmail.com>
2026-08-03 13:39:41 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1UFz8Pm5NBMHeYnlsj8ylK21veZ7Tz40",
+  authDomain: "cleaning-app-ff44b.firebaseapp.com",
+  databaseURL: "https://cleaning-app-ff44b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cleaning-app-ff44b",
+  storageBucket: "cleaning-app-ff44b.firebasestorage.app",
+  messagingSenderId: "385978909792",
+  appId: "1:385978909792:web:09824a2fbcea4358e8af05"
+};
